a2a789b44d056e38b447cec7639ebd683760de06
Kristjan Lääne <xonproject@gmail.com>
2025-05-18 16:01:23 +0300

    Account page

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,3 +1,11 @@
 export default function Page() {
-  return <div>Account</div>;
+  return (
+    <div className='text-center'>
+      <h1 className='text-3xl font-bold text-white '>Account</h1>
+
+      <p className='text-white mt-2'>
+        Logged in with email: <span className='font-bold'></span>
+      </p>
+    </div>
+  );
 }
