6da4d9244e9f051e7eec9167db9bc010bb97c734
Kristjan Lääne <xonproject@gmail.com>
2025-05-18 16:39:00 +0300

    get data from prisma

--- src/components/expenses-list.tsx
+++ src/components/expenses-list.tsx
@@ -1,18 +1,19 @@
-export default function ExpenseList() {
-  const expenses = [
-    {
-      id: 'e1',
-      description: 'Car insurance',
-      amout: 294.67,
-    },
-  ];
+type ExpenseListProps = {
+  expenses: {
+    id: number;
+    description: string;
+    amount: number;
+    createdAt: Date;
+  }[];
+};
 
+export default function ExpenseList({ expenses }: ExpenseListProps) {
   return (
     <ul className='h-[300px] bg-white rounded mt-4 shadow-md'>
       {expenses.map((expense) => (
         <li key={expense.id} className='flex items-center px-4 py-2 border-b'>
           <p>{expense.description}</p>
-          <p className='ml-auto font-bold mr-[15px]'>{expense.amout}</p>
+          <p className='ml-auto font-bold mr-[15px]'>${expense.amount}</p>
           <button className='text-[10px] h-[20px] w-[20px] bg-red-500 text-white rounded hover:bg-red-800 hover:cursor-pointer'>
             X
           </button>
